276619c000063c520f7df86208a1935e3ca970ce
amorenouc <morenopadillaalejandro@gmail.com>
2024-06-25 19:16:04 +0200

    JUL-3 HU01 Implementar la funcionalidad SM #done #time 2h 40m #comment Se ha modificado MainActivity.kt

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -58,4 +58,8 @@ class MainActivity : AppCompatActivity() {
     /*
     Simulación de la implementación de la Historia de Usuario 01 - Rol: Product Owner
      */
+
+    /*
+   Simulación de la implementación de la Historia de Usuario 01 - Rol: Scrum Master
+    */
 }
